1369cbfcb1c894d671fc1a4d6b564ad369470422
arfurlaneto <arfurlaneto@gmail.com>
2020-02-16 17:29:41 -0300

    Usable layout (not good yet).

--- src/pages/EpisodioDetail/index.js
+++ src/pages/EpisodioDetail/index.js
@@ -1,9 +1,17 @@
 import React, { useState, useCallback, useEffect, useContext } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Linking, Text, View, Image, TouchableOpacity } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import AppContext from '../../contexts/AppContext';
+
+function formatDate(date) {
+    const year = date.toString().substring(0, 4);
+    const month = date.toString().substring(4, 6);
+    const day = date.toString().substring(6, 8);
+
+    return `${day}/${month}/${year}`;
+}
 
 const EpisodeDetail = ({ route }) => {
     const { socket } = useContext(AppContext);
@@ -18,7 +26,8 @@ const EpisodeDetail = ({ route }) => {
         socket.emit('join', { episodeId: route.params.id });
 
         socket.on('episodeInfo', data => {
-            setEpisodeInfo(data);
+            console.log(data);
+            setEpisodeInfo({ ...data, date: formatDate(data.date) });
         });
 
         socket.on('myInteraction', data => {
@@ -64,14 +73,33 @@ const EpisodeDetail = ({ route }) => {
         <LinearGradient colors={['#9bcbc9', '#616161']} style={styles.container}>
             {episodeInfo && (
                 <>
-                    <Text>
-                        {episodeInfo.date} - {episodeInfo.time}
-                    </Text>
-                    <Image style={{ width: 50, height: 50 }} source={{ uri: episodeInfo.logo }} />
-                    <Text>{episodeInfo.title}</Text>
-                    <Image style={{ width: 200, height: 200 }} source={{ uri: episodeInfo.thumb }} />
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Icon name="account" style={{ fontSize: 25, color: 'black' }} />
+                            <Text style={{ fontSize: 20, color: 'black' }}>{userCount}</Text>
+                        </View>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text style={{ fontSize: 20, color: 'black' }}>
+                                {episodeInfo.date} - {episodeInfo.time}
+                            </Text>
+                        </View>
+                    </View>
+
+                    <Image
+                        resizeMode={'cover'}
+                        style={{ width: '100%', height: 200 }}
+                        source={{ uri: episodeInfo.thumb }}
+                    />
+
+                    <Text style={{ fontSize: 20, textAlign: 'center', marginTop: 5 }}>{episodeInfo.title}</Text>
                     <Text>{episodeInfo.description}</Text>
-                    <Text>{episodeInfo.link}</Text>
+                    {episodeInfo.link && (
+                        <Text style={{ color: 'blue' }} onPress={() => Linking.openURL(episodeInfo.link)}>
+                            Ver site
+                        </Text>
+                    )}
+
+                    {episodeInfo.moreInfo.length ? <Text>Mais Informações:</Text> : <></>}
                     {episodeInfo.moreInfo.map(i => (
                         <View key={i.key}>
                             <Text>
@@ -79,52 +107,58 @@ const EpisodeDetail = ({ route }) => {
                             </Text>
                         </View>
                     ))}
-                    <Text>{userCount} Usuários Assistindo</Text>
-                    <Text>Nota Média: {score || '-'}</Text>
-                    <Text>Globadas: {likes || 0}</Text>
+
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                        <Text>Nota Média: {score || '-'}</Text>
+                        <Text>Globadas: {likes || 0}</Text>
+                    </View>
+
                     {myInteraction && (
                         <>
-                            <View style={styles.starContainer}>
-                                <TouchableOpacity onPress={() => toggleStars(1)}>
-                                    <Icon
-                                        style={myInteraction.score >= 1 ? styles.star : styles.deadStar}
-                                        name="star-face"
-                                        size={24}
-                                    />
-                                </TouchableOpacity>
-                                <TouchableOpacity onPress={() => toggleStars(2)}>
-                                    <Icon
-                                        style={myInteraction.score >= 2 ? styles.star : styles.deadStar}
-                                        name="star-face"
-                                        size={24}
-                                    />
-                                </TouchableOpacity>
-                                <TouchableOpacity onPress={() => toggleStars(3)}>
-                                    <Icon
-                                        style={myInteraction.score >= 3 ? styles.star : styles.deadStar}
-                                        name="star-face"
-                                        size={24}
-                                    />
-                                </TouchableOpacity>
-                                <TouchableOpacity onPress={() => toggleStars(4)}>
-                                    <Icon
-                                        style={myInteraction.score >= 4 ? styles.star : styles.deadStar}
-                                        name="star-face"
-                                        size={24}
-                                    />
-                                </TouchableOpacity>
-                                <TouchableOpacity onPress={() => toggleStars(5)}>
-                                    <Icon
-                                        style={myInteraction.score >= 5 ? styles.star : styles.deadStar}
-                                        name="star-face"
-                                        size={24}
-                                    />
+                            <Text>Minha Avaliação</Text>
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                                <View style={{ flexDirection: 'row' }}>
+                                    <TouchableOpacity onPress={() => toggleStars(1)}>
+                                        <Icon
+                                            style={myInteraction.score >= 1 ? styles.aliveStar : styles.deadStar}
+                                            name="star-face"
+                                            size={24}
+                                        />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={() => toggleStars(2)}>
+                                        <Icon
+                                            style={myInteraction.score >= 2 ? styles.aliveStar : styles.deadStar}
+                                            name="star-face"
+                                            size={24}
+                                        />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={() => toggleStars(3)}>
+                                        <Icon
+                                            style={myInteraction.score >= 3 ? styles.aliveStar : styles.deadStar}
+                                            name="star-face"
+                                            size={24}
+                                        />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={() => toggleStars(4)}>
+                                        <Icon
+                                            style={myInteraction.score >= 4 ? styles.aliveStar : styles.deadStar}
+                                            name="star-face"
+                                            size={24}
+                                        />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={() => toggleStars(5)}>
+                                        <Icon
+                                            style={myInteraction.score >= 5 ? styles.aliveStar : styles.deadStar}
+                                            name="star-face"
+                                            size={24}
+                                        />
+                                    </TouchableOpacity>
+                                </View>
+                                <TouchableOpacity onPress={toggleLike}>
+                                    <Icon name="earth" size={24} />
+                                    {myInteraction.like ? <Text>DeGlobar!</Text> : <Text>Globar!</Text>}
                                 </TouchableOpacity>
                             </View>
-                            <TouchableOpacity onPress={toggleLike}>
-                                <Icon name="earth" size={24} />
-                                {myInteraction.like ? <Text>DeGlobar!</Text> : <Text>Globar!</Text>}
-                            </TouchableOpacity>
                         </>
                     )}
                 </>
@@ -140,11 +174,7 @@ const styles = StyleSheet.create({
         paddingTop: 40,
     },
 
-    starContainer: {
-        flexDirection: 'row',
-    },
-
-    star: {
+    aliveStar: {
         opacity: 1,
     },
 
