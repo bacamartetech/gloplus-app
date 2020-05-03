5c83c741c0cde997b9cabbb436982aa0e107dbfb
arfurlaneto <arfurlaneto@gmail.com>
2020-05-03 04:28:41 -0300

    Changed gif and base URLs

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import storage from './storage';
 
 const api = axios.create({
-    baseURL: 'http://10.0.2.2:3000',
+    baseURL: 'https://gloplus-api.glitch.me',
 });
 
 api.interceptors.request.use(
