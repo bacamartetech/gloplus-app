279e2eb0db7c0e115eced004b6516b9647b31ecf
arfurlaneto <arfurlaneto@gmail.com>
2020-05-03 02:18:12 -0300

    Current schedule is tracked correctly.

--- src/pages/EpisodioList/index.js
+++ src/pages/EpisodioList/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { StyleSheet, Text, View, Picker, FlatList, TouchableOpacity, Dimensions } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { subDays } from 'date-fns';
 
 import api from '../../services/api';
 
@@ -26,7 +27,16 @@ function getTodayValue() {
     return Number(`${year}${month}${date}`);
 }
 
+function getDateValue(today) {
+    const year = today.getFullYear();
+    const month = ('0' + (today.getMonth() + 1)).slice(-2);
+    const date = ('0' + today.getDate()).slice(-2);
+
+    return Number(`${year}${month}${date}`);
+}
+
 const EpisodioList = ({ route, navigation }) => {
+    const [todayAdjusted, setTodayAdjusted] = useState(false);
     const [emissora, setEmissora] = useState({});
     const [episodios, setEpisodios] = useState([]);
     const [selectedDate, setSelectedDate] = useState(getTodayValue());
@@ -43,47 +53,84 @@ const EpisodioList = ({ route, navigation }) => {
     const fetchEmissora = useCallback(() => {
         api.fetchEmissora(route.params.idEmissora)
             .then(response => {
-                setEmissora({
-                    ...response.data,
-                    dates: response.data.dates.map(d => ({ date: d, label: formatDate(d) })),
+                const dates = response.data.dates.map(d => ({
+                    date: d.value,
+                    label: formatDate(d.value),
+                    start: Number(d.start.slice(0, 2)) * 60 + Number(d.start.slice(3, 5)),
+                    end: Number(d.end.slice(0, 2)) * 60 + Number(d.end.slice(3, 5)),
+                }));
+
+                dates.forEach(d => {
+                    if (d.end < 1440) {
+                        d.end += 1440;
+                    }
                 });
+
+                setEmissora({ ...response.data, dates });
             })
             .catch(console.log);
     }, [route.params.idEmissora]);
 
     const fetchEpisodios = useCallback(
         date => {
-            if (date) {
-                setSelectedDate(date);
-            }
-
             api.fetchEpisodios(route.params.idEmissora, date || selectedDate)
                 .then(response => {
-                    setEpisodios(
-                        response.data.map(e => ({
-                            ...e,
-                            totalMinutes: Number(e.time.slice(0, 2)) * 60 + Number(e.time.slice(3, 5)),
-                        }))
-                    );
+                    const episodiosData = response.data.map(e => ({
+                        ...e,
+                        totalMinutes: Number(e.time.slice(0, 2)) * 60 + Number(e.time.slice(3, 5)),
+                    }));
+
+                    const maxTotalMinutes = Math.max(...episodiosData.map(e => e.totalMinutes));
+                    const maxTotalMinutesOrder = episodiosData.find(e => e.totalMinutes === maxTotalMinutes).order;
+
+                    episodiosData
+                        .filter(e => e.order > maxTotalMinutesOrder)
+                        .forEach(e => {
+                            e.totalMinutes += 1440;
+                        });
+
+                    setEpisodios(episodiosData);
                 })
                 .catch(console.log);
         },
         [route.params.idEmissora, selectedDate]
     );
 
+    useEffect(() => {
+        fetchEpisodios(selectedDate);
+    }, [fetchEpisodios, selectedDate]);
+
     const currentEpisode = useMemo(() => {
-        if (selectedDate !== getTodayValue()) {
+        if (!emissora.dates || !episodios.length) {
+            return;
+        }
+
+        const selectedInfo = emissora.dates.filter(d => d.date === selectedDate)[0];
+        let today = new Date();
+        let totalMinutes = today.getHours() * 60 + today.getMinutes();
+
+        if (totalMinutes <= selectedInfo.start) {
+            // o episódio está na programação do dia anterior
+            today = new Date(subDays(today, 1));
+            totalMinutes = today.getHours() * 60 + today.getMinutes() + 1440;
+        }
+
+        if (selectedDate !== getDateValue(today)) {
+            if (!todayAdjusted) {
+                setTodayAdjusted(true);
+                setSelectedDate(getDateValue(today));
+            }
             return _currentEpisode;
         }
 
-        const today = new Date();
-        const totalMinutes = today.getHours() * 60 + today.getMinutes();
+        if (episodios[0].date !== getDateValue(today)) {
+            return _currentEpisode;
+        }
+
         const pastEpisodes = episodios.filter(e => e.totalMinutes <= totalMinutes);
-
         _currentEpisode = pastEpisodes[pastEpisodes.length - 1];
-
         return _currentEpisode;
-    }, [episodios, selectedDate]);
+    }, [emissora.dates, episodios, selectedDate, todayAdjusted]);
 
     return (
         <LinearGradient colors={['#9bcbc9', '#616161']} style={styles.container}>
@@ -93,7 +140,7 @@ const EpisodioList = ({ route, navigation }) => {
                         <Picker
                             style={{ color: '#ffffff' }}
                             selectedValue={selectedDate}
-                            onValueChange={date => fetchEpisodios(date)}
+                            onValueChange={value => setSelectedDate(value)}
                         >
                             {emissora.dates.map(d => (
                                 <Picker.Item key={d.date} label={`Dia ${d.label}`} value={d.date} />
